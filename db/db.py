7e8d63a4d47541ad7c3d90487f3a6e684a153327
kostaskot <kostasskot@gmail.com>
2026-03-19 19:47:53 +0200

    Added class admin

--- db/db.py
+++ db/db.py
@@ -1,75 +1,153 @@
 import sqlite3
 
-def database_myBooks():
-    # ΔΗΜΙΟΥΡΓΙΑ ΒΑΣΗΣ ΔΕΔΟΜΕΝΩΝ myBooks
-    connection = sqlite3.connect("myBooks.db")
 
-    # ΕΝΕΡΓΟΠΟΙΗΣΗ ΤΩΝ ΞΕΝΩΝ ΚΛΕΙΔΙΩΝ(FOREIGN KEYS)
-    connection.execute("PRAGMA foreign_keys = ON")
+class Db_admin:
 
-    # ΔΗΜΙΟΥΡΓΙΑ ΑΝΤΙΚΕΙΜΕΝΟΥ ΚΕΡΣΟΡΑ(cursor)
-    cursor = connection.cursor()
+    def __init__(self,db_name):
+        self.db_name = db_name
+        self.db_tables()
 
-    # ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ ΧΡΗΣΤΩΝ USERS
-    create_table_users = '''CREATE TABLE IF NOT EXISTS USERS
-        (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-        )'''
-    cursor.execute(create_table_users) 
+    def create_connection(self):
+        connection = sqlite3.connect(f"{self.db_name}.db")
+        connection.execute("PRAGMA foreign_keys = ON")
+        cursor = connection.cursor()
 
-    # ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ BOOKS
+        return connection
 
-    create_table_books = '''CREATE TABLE IF NOT EXISTS BOOKS
-        (book_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        author TEXT,
-        description TEXT,
-        cover_img TEXT)'''
-    cursor.execute(create_table_books)
 
-    #ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ RATINGS
+    def db_tables(self):
+       
+       with self.create_connection() as connection:
+            # ΔΗΜΙΟΥΡΓΙΑ ΑΝΤΙΚΕΙΜΕΝΟΥ ΚΕΡΣΟΡΑ(cursor)
+            cursor = connection.cursor()
 
-    create_table_ratings ='''CREATE TABLE IF NOT EXISTS RATINGS
-        (rate_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        book_id INTEGER,
-        comments TEXT,
-        rating INTEGER CHECK (rating>=1 AND rating<=5),
-        FOREIGN KEY (user_id) REFERENCES USERS(user_id),
-        FOREIGN KEY (book_id) REFERENCES BOOKS(book_id),
-        UNIQUE(user_id,book_id)
-        )'''
+            # ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ ΧΡΗΣΤΩΝ USERS
+            create_table_users = '''CREATE TABLE IF NOT EXISTS USERS
+                (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+                )'''
+            cursor.execute(create_table_users) 
 
-    cursor.execute(create_table_ratings)
+            # ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ BOOKS
 
-    connection.commit()
-    connection.close()
+            create_table_books = '''CREATE TABLE IF NOT EXISTS BOOKS
+                (book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                author TEXT,
+                description TEXT,
+                cover_img TEXT)'''
+            cursor.execute(create_table_books)
 
-def user_registration(username, password):
+            #ΔΗΜΙΟΥΡΓΙΑ ΠΙΝΑΚΑ RATINGS
+
+            create_table_ratings ='''CREATE TABLE IF NOT EXISTS RATINGS
+                (rate_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                book_id INTEGER,
+                comments TEXT,
+                rating INTEGER CHECK (rating>=1 AND rating<=5),
+                FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+                FOREIGN KEY (book_id) REFERENCES BOOKS(book_id),
+                UNIQUE(user_id,book_id)
+                )'''
+
+            cursor.execute(create_table_ratings)
+
+            connection.commit()
+            
+
+    def user_registration(self,username, password):
     
-    connection = sqlite3.connect("myBooks.db")
-    cursor = connection.cursor()
+        with self.create_connection() as connection:
+            cursor = connection.cursor()
     
-    try:
+            try:
 
-        new_user_insert = "INSERT INTO USERS (username, password) VALUES (?, ?)"
-        cursor.execute(new_user_insert,(username, password))
-        connection.commit()
+                new_user_insert = "INSERT INTO USERS (username, password) VALUES (?, ?)"
+                cursor.execute(new_user_insert,(username, password))
+                connection.commit()
         
-        return True
+                return True
 
-    except sqlite3.IntegrityError:
-        # ΕΔΩ ΘΑ ΜΠΕΙ ΤΟ ΠΡΟΓΡΑΜΜΑ ΑΝ ΕΠΙΧΕΙΡΗΣΕΙ ΚΑΠΟΙΟΣ ΝΑ ΧΡΗΣΙΜΟΠΟΙΗΣΕΙ ΕΝΑ username ΠΟΥ ΧΡΗΣΙΜΟΠΟΙΕΙΤΑΙ ΗΔΗ
-        print(f"ΠΡΟΕΚΥΨΕ ΣΦΑΛΜΑ: {username} ΥΠΑΡΧΕΙ ΗΔΗ ΧΡΗΣΤΗΣ ΜΕ ΑΥΤΟ ΤΟ username")
-        return False
+            except sqlite3.IntegrityError:
+                # ΕΔΩ ΘΑ ΜΠΕΙ ΤΟ ΠΡΟΓΡΑΜΜΑ ΑΝ ΕΠΙΧΕΙΡΗΣΕΙ ΚΑΠΟΙΟΣ ΝΑ ΧΡΗΣΙΜΟΠΟΙΗΣΕΙ ΕΝΑ username ΠΟΥ ΧΡΗΣΙΜΟΠΟΙΕΙΤΑΙ ΗΔΗ
+                print(f"ΠΡΟΕΚΥΨΕ ΣΦΑΛΜΑ: {username} ΥΠΑΡΧΕΙ ΗΔΗ ΧΡΗΣΤΗΣ ΜΕ ΑΥΤΟ ΤΟ username")
+                return False
     
-    except Exception as fail:
-        print(f"ΑΠΟΤΥΧΙΑ: {fail}")
-        return False
+            except Exception as fail:
+                print(f"ΑΠΟΤΥΧΙΑ: {fail}")
+                return False
     
-    finally:
-        # ΣΤΟ ΤΕΛΟΣ ΚΛΕΙΝΕΙ Η ΣΥΝΔΕΣΗ ΜΕ ΤΗΝ ΒΑΣΗ
-        connection.close()
 
 
+    def find_user(self,username, password):
+        with self.create_connection() as connection:
+            cursor = connection.cursor()
+
+            search_query = ("SELECT user_id from USERS WHERE username = ? AND password = ?," (username,password))
+            cursor.execute(search_query,(username, password))
+
+            user = cursor.fetchone()
+            #ΕΠΙΣΤΡΕΦΕΙ ΤΟ user_id Ή None
+            if user:
+                return user[0] 
+            else:
+                return None
+            
+    def add_book(self,title, author, description, cover_img):
+        with self.create_connection() as connection:
+            cursor = connection.cursor()
+            try:
+                cursor.execute("SELECT book_id FROM BOOKS WHERE title = ? and author = ?",(title, author))
+                book_exists = cursor.fetchone()
+                if book_exists:
+                    print("ΤΟ ΒΙΒΛΙΟ ΕΧΕΙ ΗΔΗ ΚΑΤΑΧΩΡΗΘΕΙ ΣΤΗΝ ΒΑΣΗ")
+                    return book_exists[0]
+                
+                insert_query = "INSERT INTO BOOKS(title, author, description, cover_img) VALUES(?, ?, ?, ?)"
+                cursor.execute(insert_query,(title, author, description, cover_img))
+
+                connection.commit()
+                return cursor.lastrowid
+            except Exception as fail:
+                print(f"ΑΠΟΤΥΧΙΑ ΕΙΣΑΓΩΓΗΣ: {fail}")
+
+    def add_rating(self,user_id, book_id, comments, rating):
+        with self.create_connection() as connection:
+            cursor = connection.cursor()
+
+            if (rating < 1 or rating > 5):
+                print("ΒΑΘΜΟΛΟΓΙΑ ΠΡΕΠΕΙ ΝΑ ΕΙΝΑΙ 1-5 ")
+                return 
+            
+            try:
+                cursor.execute("SELECT rate_id FROM RATINGS WHERE user_id = ? AND book_id = ?",(user_id,book_id))
+                rating_exists = cursor.fetchone()
+                
+                if rating_exists:
+                    update_query = "UPDATE RATINGS SET comments = ?, rating = ? WHERE user_id = ? AND book_id = ?"
+                    cursor.execute(update_query,(comments, rating, user_id, book_id))
+                    print("ΕΠΙΤΥΧΗΣ ΕΝΗΜΕΡΩΣΗ ΚΑΤΑΧΩΡΗΣΗΣ")
+                else:
+                    insert_query = "INSERT INTO RATINGS(user_id, book_id, comments, rating) VALUES (?, ?, ?, ?)"
+                    cursor.execute(insert_query,(user_id, book_id, comments, rating))
+                    print("ΕΠΙΤΥΧΗΣ ΑΞΙΟΛΟΓΗ ΒΙΒΛΙΟΥ")
+
+                connection.commit()
+            except sqlite3.IntegrityError as Error:
+                print(f"ΠΡΟΕΚΥΨΕ ΑΚΕΡΑΙΟΤΗΤΑΣ: {Error}")
+                return False
+            except Exception as fail:
+                print(f"ΑΠΟΤΥΧΙΑ: {fail}")
+                return False
+        
+
+            
+
+if __name__== "__main__":
+
+    db_name = 'myBooks'
+    myBooks_db = Db_admin(db_name)
+    
+    
