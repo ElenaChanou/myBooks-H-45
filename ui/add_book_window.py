7922783a3ca01f162389992dcf085e7cfd699ad4
giannisbalkanas <giannisbalkanaw@gmail.com>
2026-06-03 19:50:58 +0300

    Διόρθωση: Δυναμικό user_id στις αξιολογήσεις, σύνδεση κουμπιών Δημοφιλή και Δημοφιλη αδιάβαστα και διόρθωση κλήσης get_user_ratings

--- ui/add_book_window.py
+++ ui/add_book_window.py
@@ -1,14 +1,19 @@
 import customtkinter as ctk
 from ui.manual_book_window import ManualBookWindow
-from services.import_service import search_books_online
+from services.import_service import search_books_online, import_online_book
 
 class AddBookWindow(ctk.CTkToplevel):
-    def __init__(self,parent):
+    def __init__(self,parent, on_add_success=None):
         super().__init__(parent)
+
+        # Αποθηκεύουμε τη συνάρτηση ανανέωσης για να τη χρησιμοποιήσουμε στο τέλος
+        self.on_add_success = on_add_success
 
         self.title("Προσθήκη Βιβλίου")
         self.geometry("500x800")
         self.grab_set()
+        #προσθήκη λίστας με τα 4 πρώτα αποτελέσματα
+        self.current_results = []
         self.create_widgets()
         
     def create_widgets(self):
@@ -32,7 +37,11 @@ class AddBookWindow(ctk.CTkToplevel):
         self.button_search_add = ctk.CTkButton(self.main_container, text="Αναζήτηση και Προσθήκη", command = self.search_and_add)
         self.button_search_add.grid(row=3, column = 0, padx=5, pady=(10,5))
 
-    # --- Η συνάρτηση ήρθε πιο αριστερά, στην ίδια ευθεία με το create_widgets ---
+        #Προσθήκη label και dropdown για να επιλέξει ο χρήστης ένα από τα τέσσερα βιβλία
+        self.label_dropdown = ctk.CTkLabel(self.main_container, text = "Επιλέξτε το σωστό βιβλίο:")
+        self.option_menu = ctk.CTkOptionMenu(self.main_container, values=[], command=self.on_book_selected, width=250)
+
+    
     def search_and_add(self):
         title = self.entry_title.get().strip()
         
@@ -46,14 +55,64 @@ class AddBookWindow(ctk.CTkToplevel):
         results = search_books_online(title)
 
         if results:
+            self.current_results=results[:4]
             print(f"Επιτυχία! Βρέθηκαν {len(results)} αποτελέσματα στο ίντερνετ.")
-            # Εκτυπώνουμε το πρώτο αποτέλεσμα για να δούμε αν δουλεύει
-            print("ΠΡΩΤΟ ΑΠΟΤΕΛΕΣΜΑ:", results[0]['title'], "-", results[0]['authors'])
             
+            #θα χρησιμοποιήσουμε το .get(). Αυτό λέει στην Python: να φερει  τον συγγραφέα, αλλά αν δεν υπάρχει να μην κρασάρει απλά θα τυπώνει 'Άγνωστος'".
+            
+            dropdown_options = []
+            for book in self.current_results:
+                title = book.get('title', 'Αγνωστος συγγραφέας')
+                author = book.get('authors', 'Άγνωστος Συγγραφέας')
+                print(f"ΒΡΕΘΗΚΕ: {title} - {author}")
+                dropdown_options.append(f"{title} ({author})")
+            #Με το configure ταΐζουμε το κρυφό μας μενού με τη λίστα που μόλις φτιάξαμε. Το set του βάζει έναν προεπιλεγμένο τίτλο. Τέλος, οι εντολές grid παίρνουν το μενού και την ταμπέλα του από την "αφάνεια" και τα καρφιτσώνουν στην οθόνη.
+            self.option_menu.configure(values=dropdown_options)
+            self.option_menu.set("---Κάντε μια επιλογή---")
+            ## Εμφανίζουμε το μενού στην οθόνη ΜΟΝΟ εφόσον βρέθηκαν βιβλία και το γεμίσαμε.
+            self.label_dropdown.grid(row=4, column=0, pady=(15,5))
+            self.option_menu.grid(row=5, column=0, pady=5)
+
             # Στο μέλλον, εδώ θα καλούμε την import_online_book για αποθήκευση
+            
         else:
             print(f"Το βιβλίο '{title}' ΔΕΝ βρέθηκε. Άνοιγμα χειροκίνητης εισαγωγής...")
+            #Αν ο χρήστης είχε ψάξει πριν κάτι σωστό (και το μενού ήταν ανοιχτό), και μετά ψάξει κάτι λάθος, δεν θέλουμε να βλέπει το παλιό μενού. Το grid_forget() είναι το αντίθετο του grid(): το εξαφανίζει από την οθόνη.
+            self.label_dropdown.grid_forget()
+            self.option_menu.grid_forget
             ManualBookWindow(self)
+
+    def on_book_selected(self, choice):
+        # Αν ο χρήστης κάνει κλικ στον αρχικό τίτλο του μενού, μην κάνεις τίποτα
+        if choice == "--- Κάντε μια επιλογή ---":
+            return
+            
+        # 1. Βρίσκουμε σε ποια θέση (0, 1, 2, 3) της λίστας αντιστοιχεί αυτό που πάτησε
+        selected_index = self.option_menu._values.index(choice)
+        
+        # 2. Τραβάμε ολόκληρο το βιβλίο από τη "μνήμη" μας χρησιμοποιώντας αυτή τη θέση
+        selected_book = self.current_results[selected_index]
+        
+        print(f"Ο χρήστης επέλεξε το: {selected_book.get('title')}")
+
+        self.entry_title.delete(0, "end")
+        self.entry_title.insert(0, selected_book.get('title',""))
+
+        self.entry_author.delete(0, "end")
+        self.entry_author.insert(0, selected_book.get('title',""))
+
+        try:
+            book_id=import_online_book(selected_book)
+            print("Αποθηκεύτηκε επιτυχώς το βιβλίο με ID:", book_id)
+            self.label_dropdown.grid_forget()
+            self.option_menu.grid_forget()
+            #Ενημέρωση του κεντρικού να ξαναδιαβάσει τη βάση
+            if self.on_add_success:
+                self.on_add_success()
+        except Exception as e:
+            print(f"Σφάλμα κατά την αποθήκευση: {e}")
+
+
 
 if __name__ == "__main__":
     # Φτιάχνουμε ένα αόρατο/μικρό βασικό παράθυρο-γονιό για να πατήσει πάνω του το δικό μας
--- ui/book_details_window.py
+++ ui/book_details_window.py
@@ -4,14 +4,19 @@ from PIL import Image,ImageTk
 import os
 from tkinter import messagebox
 import customtkinter as ctk
-#from api_man import load_image_from_url
+from services.rating_service import save_rating
 
+
+
+#Σύμφωνα με το book_service και τη συνάρτηση unread_popular_books, θεωρείται διαβασμένο το βιβλίο 
+#μόνο αν έχει προστεθεί κριτική.
 
 class BookDetailsWindow(ctk.CTkFrame):
     def __init__(self,master, book_data, on_save):#αλλαγή στο μέλλον θα μπει μια παράμετρος book_data που θα λαβάνει από το main_window
         #Αρχικοποίηση ως frame
         super().__init__(master, fg_color="transparent")
-        self.book = book_data
+        self.book = book_data.get("book", {})
+        self.ratings = book_data.get("ratings", [])
         self.on_save = on_save
 
        
@@ -22,10 +27,10 @@ class BookDetailsWindow(ctk.CTkFrame):
                      #"authors": "Μ.Καραγάτσης",
                      #"cover_path": None}
         #τα ratings θα έρχονται από το api
-        self.ratings = [
-            {"username": "Giannis", "rating": 5, "comment": "Εξαιρετικό!"},
-            {"username": "Vasilis", "rating": 4, "comment": "Αρκετά καλό"}
-        ]
+        #self.ratings = [
+         #   {"username": "Giannis", "rating": 5, "comment": "Εξαιρετικό!"},
+          #  {"username": "Vasilis", "rating": 4, "comment": "Αρκετά καλό"}
+        #]
         self.create_widgets()
 
     def create_widgets(self):
@@ -40,7 +45,7 @@ class BookDetailsWindow(ctk.CTkFrame):
         self.label_title=ctk.CTkLabel(self, text=self.book["title"], font=("arial", 18,"bold"))
         self.label_title.grid(row=1, column=0, pady=(10, 15))
 
-        self.label_authors=ctk.CTkLabel(self, text=f"Συγγραφέας:{self.book['author']}", font= ("Arial", 12))
+        self.label_authors=ctk.CTkLabel(self, text=f"Συγγραφέας:{self.book.get('authors', 'Άγνωστος')}", font= ("Arial", 12))
         self.label_authors.grid(row=2, column=0,pady=5)
 
         print("--- ΔΕΔΟΜΕΝΑ ΠΟΥ ΕΦΤΑΣΑΝ ΣΤΙΣ ΛΕΠΤΟΜΕΡΕΙΕΣ ---")
@@ -114,7 +119,7 @@ class BookDetailsWindow(ctk.CTkFrame):
 
         #Φόρτωση σχολίων από Demo Data
         for r in self.ratings:
-            display_text = f"⭐ {r['rating']}/5 - {r['username']}: {r['comment']}"
+            display_text = f"⭐ {r.get('rating',0)}/5 - {r.get('username', 'Άγνωστος')}: {r.get('comment', '')}"
             self.tree_comments.insert("", "end", values=(display_text,))
 
     def handle_save(self):
@@ -128,11 +133,34 @@ class BookDetailsWindow(ctk.CTkFrame):
         if not rating:
             messagebox.showwarning("Προσοχή","Παρακαλώ επιλέξτε βαθμολογία!")
             return
-        print(f"Αποθήκευση: rating={rating}, comment={comment}")
-        messagebox.showinfo("Επιτυχία", "Η αξιολόγησή σου αποθηκεύτηκε!")
+        # Παίρνουμε το ID του βιβλίου από τα δεδομένα
+        book_id = self.book.get('book_id')
+        
+       # Παίρνουμε το αληθινό ID του συνδεδεμένου χρήστη από τον controller
+        user_id = self.master.current_user.get('id') 
+        
+        try:
+            # Καλούμε την πραγματική συνάρτηση της βάσης
+            save_rating(user_id, book_id, int(rating), comment)
+            
+            messagebox.showinfo("Επιτυχία", "Η αξιολόγησή σου αποθηκεύτηκε!")
+            print(f"Αποθηκεύτηκε πραγματικά στη βάση: rating={rating}, comment={comment}")
+            
+            # Αδειάζουμε το κουτάκι του κειμένου (UX)
+            self.text_comment.delete("1.0", tk.END)
+            
+            # Ενημερώνουμε τον κεντρικό πίνακα στο παρασκήνιο
+            if self.on_save:
+                self.on_save()
+
+        except Exception as e:
+            messagebox.showerror("Σφάλμα", f"Δεν μπόρεσε να αποθηκευτεί: {e}")
+       
 
         self.on_save()
-        self.go_back() # Χρησιμοποιούμε τη go_back για να κλείσει το frame
+        #self.go_back() # Χρησιμοποιούμε τη go_back για να κλείσει το frame
+    
+   
 
     def go_back(self):
         print("Επιστροφή στην αρχική οθόνη!")
